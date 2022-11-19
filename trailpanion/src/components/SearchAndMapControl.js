93cf4474f19dd0b647c0c7dca583e7bdb85ca440
Louie Knolle <knollelw@gmail.com>
2022-11-19 17:21:24 -0500

    practice map styles and start looking into geocoding

--- trailpanion/src/components/SearchAndMapControl.js
+++ trailpanion/src/components/SearchAndMapControl.js
@@ -1,20 +1,32 @@
 import SearchForm from "./SearchForm";
 import HomeMap from "./HomeMap";
-import React, { useState } from "react";
-// import getTrailsData from '../getTrailsData';
+import React, { useState, useEffect } from "react";
+import TrailResults from "./TrailResults";
+import { getTrailsData } from "../getTrailsData";
 
 const SearchAndMapControl = () => {
   const [data, setData] = useState({});
+  const [results, setResults] = useState([]);
 
   const onSubmitSearch = (newSearch) => {
     console.log(data);
     setData(newSearch);
   };
 
+  useEffect(() => {
+    getTrailsData().then((response) => {
+      const responseArray = Object.values(response);
+      setResults(responseArray);
+    });
+  }, []);
+
+  console.log(results);
+
   return (
     <React.Fragment>
       <SearchForm onSearchTrails={onSubmitSearch} />
       <HomeMap />
+      <TrailResults />
     </React.Fragment>
   );
 };
