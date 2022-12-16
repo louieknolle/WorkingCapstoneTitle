2060691bb6fe133acc5f49afcfb86bb32249815e
Louie Knolle <knollelw@gmail.com>
2022-12-15 20:20:10 -0500

    start making schemas, routes and controllers for other gear categories

---
+++ backend/models/footwearModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const footwearSchema = new Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    style: {
+      type: String,
+      required: true,
+    },
+    waterproof: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Footwear", footwearSchema);
